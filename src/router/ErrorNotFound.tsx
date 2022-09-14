96aa196c0e169ff5a0c7b709926b1742796810fa
lopo <lopo@zju.edu.cn>
2022-09-14 23:23:43 +0800

    feat: 错误页及其信息

--- src/router/ErrorNotFound.tsx
+++ src/router/ErrorNotFound.tsx
@@ -1,8 +1,14 @@
-import { IconNotFound } from "../icons";
+import { IconError } from "../icons";
 import "./ErrorNotFound.scss";
+import { useRouteError } from "react-router-dom";
 
 export default () => {
+    const error = useRouteError() as any
+    console.error(error)
     return <div className="error-not-found">
-        <IconNotFound className="spin"/>
+        <IconError className="spin"/>
+        <div className="error-msg">
+            Error: { error.statusText || error.message || '查看控制台获取错误信息!' }
+        </div>
     </div>
 }
