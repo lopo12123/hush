dc8915724c1fb6ffd178847b7cfd5dd9f1b68cd7
lopo <lopo@zju.edu.cn>
2022-09-14 22:52:43 +0800

    feat: 404配置

---
+++ src/router/ErrorNotFound.tsx
@@ -0,0 +1,8 @@
+import { IconError } from "../icons";
+import "./ErrorNotFound.scss";
+
+export default () => {
+    return <div className="error-not-found">
+        <IconError className="spin"/>
+    </div>
+}
