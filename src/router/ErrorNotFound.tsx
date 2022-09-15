5cbb1090d2f1925ab98b60f698b5a77ab24f434f
lopo <lopo@zju.edu.cn>
2022-09-15 21:23:37 +0800

    feat: 错误页处理

--- src/router/ErrorNotFound.tsx
+++ src/router/ErrorNotFound.tsx
@@ -1,14 +1,29 @@
 import { IconError } from "../icons";
-import "./ErrorNotFound.scss";
-import { useRouteError } from "react-router-dom";
+import Styles from "./ErrorNotFound.module.scss";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default () => {
+    const navigator = useNavigate()
     const error = useRouteError() as any
     console.error(error)
-    return <div className="error-not-found">
-        <IconError className="spin"/>
-        <div className="error-msg">
+    return <div className={ Styles.errorNotFound }>
+        <IconError className={ Styles.spin }/>
+        <div className={ Styles.errorMsg }>
             Error: { error.statusText || error.message || '查看控制台获取错误信息!' }
+        </div>
+        <div className={ Styles.operations }>
+            <div title="返回上一页"
+                 onClick={ () => {
+                     navigator(-1)
+                 } }>
+                Previous
+            </div>
+            <div title="返回首页"
+                 onClick={ () => {
+                     navigator('/')
+                 } }>
+                HomePage
+            </div>
         </div>
     </div>
 }
