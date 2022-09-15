1dc9525e32fe5d13ee98fbd99586d5b5053ac774
lopo <lopo@zju.edu.cn>
2022-09-15 21:32:16 +0800

    feat: 页面重定向

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import Styles from "./App.module.scss";
 import { Suspense, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { hashRoutes } from "./router/hash";
 import MenuItem from "./components/MenuItem";
 
 export default () => {
+    const navigator = useNavigate()
     const [ activeIdx, setActiveIdx ] = useState(0)
+
     return (
         <div className={ Styles.app }>
             <Toaster/>
             <div className={ Styles.menu }>
                 {
                     hashRoutes.map((route, idx) => {
+                        // 重定向不用渲染菜单
+                        if(route.path === '') return;
+
+                        // 一个子页面一个菜单项
                         return <MenuItem
                             key={ idx } detail={ route }
                             active={ activeIdx === idx }
                             onClick={ () => {
                                 setActiveIdx(idx)
-                                // todo
-                                window.location.hash = '#/' + route.path!
+                                navigator(route.path!)
                             } }/>
                     })
                 }
--- src/router/hash.tsx
+++ src/router/hash.tsx
@@ -1,16 +1,21 @@
 import { lazy } from "react";
-import { RouteObject } from "react-router-dom";
+import { Navigate, RouteObject } from "react-router-dom";
 
 const Md5 = lazy(() => import("../views/hash/Md5"))
 const Sha256 = lazy(() => import("../views/hash/Sha256"))
 
 export const hashRoutes: RouteObject[] = [
     {
+        path: 'md5',
+        element: <Md5/>
+    },
+    {
         path: 'sha256',
         element: <Sha256/>
     },
     {
-        path: 'md5',
-        element: <Md5/>
+        // 进入页面的默认跳转
+        path: "",
+        element: <Navigate to="md5" replace/>
     }
 ]
