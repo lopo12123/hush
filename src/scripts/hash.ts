ef5a4c4ad190d1e79dffac57cc1154da5069e178
lopo <lopo@zju.edu.cn>
2022-09-21 13:51:17 +0800

    feat: 封装通用

--- src/scripts/hash.ts
+++ src/scripts/hash.ts
@@ -1,13 +1,20 @@
 import { enc } from "crypto-js";
 import WordArray from "crypto-js/lib-typedarrays";
+import MD5 from "crypto-js/md5";
+import SHA1 from "crypto-js/sha1";
+import SHA256 from "crypto-js/sha256";
+import SHA224 from "crypto-js/sha224";
+import SHA384 from "crypto-js/sha384";
+import SHA512 from "crypto-js/sha512";
+import SHA3 from "crypto-js/sha3";
+import RIPEMD160 from "crypto-js/ripemd160";
 
 // region hash及输出编码
 // hash类型
-export type HashType = 'Md5' | 'sha1'
+export type HashType = 'md5' | 'sha1'
     | 'sha256' | 'sha512' | 'sha224' | 'sha384'
     | 'sha3_256' | 'sha3_512' | 'sha3_224' | 'sha3_384'
     | 'ripemd160'
-
 // 编码类型
 export type EncodeType =
     'Base64' | 'Base64Url' | 'Hex' | 'Latin1' |
@@ -51,14 +58,40 @@ export function readFileToType(target: TargetType): Promise<ArrayBuffer | string
 
 // endregion
 
-// region 文件(ArrayBuffer)哈希
+// region 哈希// 输入 => WordArray
+const Source2Hash: { [k in HashType]: (message: WordArray | string) => WordArray } = {
+    md5: MD5,
+    sha1: SHA1,
+    sha256: SHA256,
+    sha512: SHA512,
+    sha224: SHA224,
+    sha384: SHA384,
+    sha3_256: message => SHA3(message, { outputLength: 256 }),
+    sha3_512: message => SHA3(message, { outputLength: 512 }),
+    sha3_224: message => SHA3(message, { outputLength: 224 }),
+    sha3_384: message => SHA3(message, { outputLength: 384 }),
+    ripemd160: RIPEMD160
+}
+// WordArray => 输出字符串
+const Hash2Result: { [k in EncodeType]: (wa: WordArray) => string } = {
+    Base64: wa => wa.toString(ResultEncoder['Base64']),
+    Base64Url: wa => wa.toString(ResultEncoder['Base64Url']),
+    Hex: wa => wa.toString(ResultEncoder['Hex']),
+    Latin1: wa => wa.toString(ResultEncoder['Latin1']),
+    Utf8: wa => wa.toString(ResultEncoder['Utf8']),
+    Utf16: wa => wa.toString(ResultEncoder['Utf16']),
+    Utf16BE: wa => wa.toString(ResultEncoder['Utf16BE']),
+    Utf16LE: wa => wa.toString(ResultEncoder['Utf16LE']),
+}
 // 文件(ArrayBuffer)哈希
-export const hashFile = (file: ArrayBuffer, hash: HashType, encode: EncodeType = 'Hex',) => {
+export const hashFile = (file: ArrayBuffer, type: HashType, encode: EncodeType = 'Hex',) => {
     // @ts-ignore
     const _wa = WordArray.create(file)
 
-    // todo do-hash
-
-    return _wa.toString(ResultEncoder[encode])
+    return Hash2Result[encode](Source2Hash[type](_wa))
+}
+// 字符串(string)哈希
+export const hashString = (str: string, type: HashType, encode: EncodeType = 'Hex') => {
+    return Hash2Result[encode](Source2Hash[type](str))
 }
 // endregion
